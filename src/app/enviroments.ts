981dae67329a79af1716f4cb33f07118eccea3ef
AttilaJager <jagerattila@gmail.com>
2024-02-03 13:43:03 +0100

    first commit

---
+++ src/app/enviroments.ts
@@ -0,0 +1,13 @@
+export const Enviroments = {
+    production:false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBpT9KSgGhIIzMi2_ZPSDRyrP9Mtj4s8uc",
+        authDomain: "novenyek-2ac0d.firebaseapp.com",
+        databaseURL: "https://novenyek-2ac0d-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "novenyek-2ac0d",
+        storageBucket: "novenyek-2ac0d.appspot.com",
+        messagingSenderId: "189293208803",
+        appId: "1:189293208803:web:e8c08c557ce1c2d12651d6",
+        measurementId: "G-MMGSSLY6T8"
+    }
+}
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { Noveny } from '../model/noveny';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+
+  refNovenyek: AngularFireList<Noveny>
+  refMegrendeles: AngularFireList<any>
+
+  constructor(private db:AngularFireDatabase) {
+    this.refNovenyek= this.db.list("/novenyek")
+    this.refMegrendeles= this.db.list("/megrendelesek")
+   }
+
+  getPlants(){
+    return this.refNovenyek
+  }
+  megrendel(body:any){      
+  return this.refMegrendeles.push(body)
+  }
+}
